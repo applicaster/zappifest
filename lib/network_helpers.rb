6b331dda604670f150ec9bb289273bc32019a03e
ranmeirman <r.meirman@applicaster.com>
2016-09-20 10:09:08 -0400

    add identifier to plugin (#22)

--- lib/network_helpers.rb
+++ lib/network_helpers.rb
@@ -33,6 +33,7 @@ module NetworkHelpers
       params["plugin[name]"] = "#{manifest_data['name']} - #{manifest_data['platform']}"
       params["plugin[author_email]"] = manifest_data["author_email"]
       params["plugin[category]"] = manifest_data["type"]
+      params["plugin[identifier]"] = manifest_data["identifier"]
     end
   end
 end
