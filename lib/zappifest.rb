d3cc94b948c4ea5a2c4b279f9569cc591057424c
ranmeirman <r.meirman@applicaster.com>
2017-02-15 23:35:37 -0500

    move react dependencies to mandatory fields (#43)

--- lib/zappifest.rb
+++ lib/zappifest.rb
@@ -97,23 +97,34 @@ command :init do |c|
       end
     end
 
-    manifest_hash[:custom_configuration_fields] = []
-
     manifest_hash[:react_native] = agree "[?] React Native plugin? (Y/n)"
 
     if manifest_hash[:react_native]
-      default = Question.ask_base "Default dependencies: (comma seperated)"
-      extra_dependencies = { key: "extra_dependencies", type: :tags, default: default.gsub!(" ", "") }
-      manifest_hash[:custom_configuration_fields].push(extra_dependencies)
+      manifest_hash[:extra_dependencies] = []
 
-      default = Question.ask_base "Default npm dependencies: (comma seperated)"
-      npm_dependencies = { key: "npm_dependencies", type: :tags, default: default.gsub!(" ", "") }
-      manifest_hash[:custom_configuration_fields].push(npm_dependencies)
+      extra_dependencies_count = ask("[?] Number of extra dependencies: ", Integer) { |q| q.in = 1..10 }
+
+      extra_dependencies_count.times do |index|
+        dependency = {}
+        color "Dependency #{index + 1}", :yellow
+        color "---------------------", :yellow
+
+        name = Question.ask_non_whitespaces("Dependency Name:", "Dependency Name")
+        description = manifest_hash[:platform] == :android ? "e.g. 1.0, 4.8+, etc." : "e.g. ~> 1.0, >= 3.0, :path => 'path/to/dependency', etc."
+        parameters = Question.ask_base("Dependency Parameters: (#{description})")
+        dependency[name] = parameters
+        manifest_hash[:extra_dependencies].push(dependency)
+        color "#{name} dependency added!", :green
+      end
+
+      manifest_hash[:npm_dependencies] = ask "[?] NPM dependencies: (e.g. module@0.38.0 or blank line to continue)" do |q|
+        q.gather = ""
+      end
 
       if manifest_hash[:platform] == :android
-        default = Question.ask_base "React Packages: (comma seperated)"
-        react_packages = { key: "react_packages", type: :tags, default: default.gsub!(" ", "") }
-        manifest_hash[:custom_configuration_fields].push(react_packages)
+        manifest_hash[:react_packages] = ask "[?] React Packages: (or blank line to quit)" do |q|
+          q.gather = ""
+        end
       end
     end
 
@@ -121,6 +132,8 @@ command :init do |c|
     add_custom_fields = agree "[?] Wanna add custom fields? "
 
     if add_custom_fields
+      manifest_hash[:custom_configuration_fields] = []
+
       custom_fields_count = ask("[?] How many? ", Integer) { |q| q.in = 1..10 }
 
       custom_fields_count.times do |index|
