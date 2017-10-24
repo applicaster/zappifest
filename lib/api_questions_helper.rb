1af03d25912b0fc88b9330480c9ee23a88f2d090
ranmeirman <r.meirman@applicaster.com>
2017-10-24 13:37:39 -0400

    feat(api): adding swift modules to support multiple modules (#70)

--- lib/api_questions_helper.rb
+++ lib/api_questions_helper.rb
@@ -8,11 +8,15 @@ module ApiQuestionsHelper
       "By setting to true, the plugin must implement app startup interface"
 
     manifest_hash[:api][:class_name] = Question.ask_non_empty("Class Name:", "Class Name")
-    return manifest_hash unless manifest_hash[:platform].to_s =~ /android/
 
-    add_proguard_rules = agree "[?] Need to add custom Proguard rules? (will open a text editor)"
-    if add_proguard_rules
-      manifest_hash[:api][:proguard_rules] = ask_editor(nil, "vim")
+    if manifest_hash[:platform].to_s =~ /android/
+      add_proguard_rules = agree "[?] Need to add custom Proguard rules? (will open a text editor)"
+      manifest_hash[:api][:proguard_rules] = ask_editor(nil, "vim") if add_proguard_rules
+
+    elsif manifest_hash[:platform].to_s =~ /ios/
+      manifest_hash[:api][:module_names] = ask "[?] Enter Swift module names the plugin should support (or blank line to quit)" do |q|
+        q.gather = ""
+      end
     end
 
     manifest_hash
