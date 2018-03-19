8f40c2b25f97bf4972af663f8b13155b0f23cdd4
ranmeirman <r.meirman@applicaster.com>
2018-03-19 14:55:55 -0400

    chore(nav items): add selector for supported nav items on Navigation types plugins (#98)

---
+++ lib/navigation_plugins_questions_helper.rb
@@ -0,0 +1,28 @@
+module NavigationPluginsQuestionsHelper
+  module_function
+
+  def ask_nav_items(manifest_hash)
+    return manifest_hash unless manifest_hash[:type].to_s == "menu" ||  manifest_hash[:type].to_s == "nav_bar"
+
+    booleans_array = Ask.checkbox(
+      "[?] Select supported navigation items (use the spacebar for multiple)", navigation_items_mapper.values
+    )
+
+    nav_items_indices = booleans_array.each_index.select { |i| booleans_array[i] }
+    manifest_hash[:supported_nav_items]  = navigation_items_mapper.keys.values_at(*nav_items_indices)
+    manifest_hash
+  end
+
+  def navigation_items_mapper
+    {
+      nav_header: "Header",
+      nav_second_level: "Nested Menu",
+      nav_screen: "Screen",
+      nav_url: "URL",
+      chromecast: "Chromecast",
+      applicaster_feed: "Applicaster Social Feed",
+      applicaster_crossmates: "Crossmates",
+      live_drawer: "Live Drawer",
+    }
+  end
+end
