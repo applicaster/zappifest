6a391b9b486f4b131711d794d79556fca7c1f483
Yuli Roth <y.roth@applicaster.com>
2019-06-23 16:40:42 +0300

    addressed the comments

--- .bundle/gems/blockenspiel-0.5.0/test/files/file1.rb
+++
@@ -1,2 +0,0 @@
-set_value(:a, 1)
-set_value_by_block(:b){ 2 }
--- .bundle/gems/inquirer-0.2.1/example/confirm.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/inquirer'
-
-value1 = Ask.confirm "Are you sure?"
-value2 = Ask.confirm "Are you really sure?", default: false
-puts "you chose #{value1} and #{value2}"
