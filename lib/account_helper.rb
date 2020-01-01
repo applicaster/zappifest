9734e4517a2ac941e307e4bffd4fa11dd6183f69
Yuli Roth <y.roth@applicaster.com>
2020-01-01 15:25:18 +0200

    adressed the comments

--- lib/account_helper.rb
+++ lib/account_helper.rb
@@ -1,14 +1,17 @@
 class AccountHelper
     include NetworkHelpers
-  
+
     def initialize(user, account)
       @user = user
       @account = account
     end
-  
-    def valid_account?(options)
-      existing_account = NetworkHelpers.get_accounts_list(options).body.find { |account| account["old_id"] == @account } 
 
+    def valid_account?(options)
+      accounts_list = NetworkHelpers.get_accounts_list(options).body
+
+      return false unless accounts_list.is_a? Array
+
+      existing_account = accounts_list.find { |account| account["old_id"] == @account }
       existing_account.present?
     end
 
@@ -16,7 +19,7 @@ class AccountHelper
       account_permission = @user["permissions"].find { |perm| perm["account_id"] == @account }
 
       return unless account_permission.present?
-      
+
       return account_permission["roles"].find { |role| role == "zapp:plugin_developer" }.present?
     end
   end
