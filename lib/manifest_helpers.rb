e4dc80b0ec8d76b65182ab5239e4658d2c8c3177
Yuli <y.roth@applicaster.com>
2020-03-23 12:47:36 +0200

    feat(OPTIONAL_KEYS): adds characteristic (#171)
    * feat(OPTIONAL_KEYS): adds characteristic
    
    * changes to characteristics

--- lib/manifest_helpers.rb
+++ lib/manifest_helpers.rb
@@ -1,109 +1,110 @@
 module ManifestHelpers
   NEW_PLUGIN_MANDATORY_KEYS = [
-    "whitelisted_account_ids",
-  ]
+    'whitelisted_account_ids'
+  ].freeze
 
-  MANDATORY_KEYS = [
-    "author_name",
-    "author_email",
-    "manifest_version",
-    "name",
-    "description",
-    "identifier",
-    "type",
-    "min_zapp_sdk",
-    "ui_builder_support",
-  ]
+  MANDATORY_KEYS = %w[
+    author_name
+    author_email
+    manifest_version
+    name
+    description
+    identifier
+    type
+    min_zapp_sdk
+    ui_builder_support
+  ].freeze
 
-  OPTIONAL_KEYS = [
-    "api",
-    "platform",
-    "dependency_name",
-    "dependency_version",
-    "dependency_repository_url",
-    "deprecated_since_zapp_sdk",
-    "unsupported_since_zapp_sdk",
-    "custom_configuration_fields",
-    "react_native",
-    "extra_dependencies",
-    "project_dependencies",
-    "npm_dependencies",
-    "scheme",
-    "data_types",
-    "zapp_pipes",
-    "react_bundle_url",
-    "thumbnail",
-    "screenshots",
-    "preview",
-    "about",
-    "general",
-    "assets",
-    "styles",
-    "rules",
-    "data",
-    "advertising",
-    "screen",
-    "supported_nav_items",
-    "max_nav_items",
-    "max_items_per_type",
-    "zapp_configuration",
-    "summary",
-    "cover_image",
-    "guide",
-    "core_plugin",
-    "configuration_panel_disabled",
-    "export",
-    "import",
-    "hooks",
-    "preload",
-    "postload",
-    "supports_offline",
-    "targets",
-    "localizations",
-    "ui_frameworks",
-  ]
+  OPTIONAL_KEYS = %w[
+    api
+    platform
+    dependency_name
+    dependency_version
+    dependency_repository_url
+    deprecated_since_zapp_sdk
+    unsupported_since_zapp_sdk
+    custom_configuration_fields
+    react_native
+    extra_dependencies
+    project_dependencies
+    npm_dependencies
+    scheme
+    data_types
+    zapp_pipes
+    react_bundle_url
+    thumbnail
+    screenshots
+    preview
+    about
+    general
+    assets
+    styles
+    rules
+    data
+    advertising
+    screen
+    supported_nav_items
+    max_nav_items
+    max_items_per_type
+    zapp_configuration
+    summary
+    cover_image
+    guide
+    core_plugin
+    configuration_panel_disabled
+    export
+    import
+    hooks
+    preload
+    postload
+    supports_offline
+    targets
+    localizations
+    ui_frameworks
+    characteristics
+  ].freeze
 
   Types = [
-    { label: "Player", value: "player", platform_required: true },
-    { label: "Navigation", value: "menu", platform_required: true, screen: false },
-    { label: "Navigation Bar", value: "nav_bar", platform_required: true, screen: false },
-    { label: "Analytics Provider", value: "analytics", platform_required: true, screen: false },
-    { label: "Article", value: "article", platform_required:true, screen: true },
-    { label: "Advertisement", value: "advertisement", platform_required: true, screen: false },
-    { label: "Payments", value: "payments", screen: false },
-    { label: "Authentication", value: "auth_provider", platform_required: true },
-    { label: "Push Provider", value: "push_provider", platform_required: true, screen: false },
-    { label: "Ui Component", value: "ui_component", platform_required: true, screen: false },
-    { label: "Login", value: "login", platform_required: true },
-    { label: "Data Source Provider", value: "data_source_provider", platform_required: false, screen: false },
-    { label: "Broadcaster Selector", value: "broadcaster_selector", platform_required: true },
-    { label: "General", value: "general", platform_required: true },
-    { label: "Cell Style Family", value: "cell_style_family", platform_required: true },
-    { label: "Cell Builder", value: "cell_builder", platform_required: true, screen: false },
-    { label: "Video Advertisement", value: "video_advertisement", platform_required: true, screen: false },
-    { label: "Player Overlay", value: "player_overlay", platform_required: false, screen: false },
-  ]
+    { label: 'Player', value: 'player', platform_required: true },
+    { label: 'Navigation', value: 'menu', platform_required: true, screen: false },
+    { label: 'Navigation Bar', value: 'nav_bar', platform_required: true, screen: false },
+    { label: 'Analytics Provider', value: 'analytics', platform_required: true, screen: false },
+    { label: 'Article', value: 'article', platform_required: true, screen: true },
+    { label: 'Advertisement', value: 'advertisement', platform_required: true, screen: false },
+    { label: 'Payments', value: 'payments', screen: false },
+    { label: 'Authentication', value: 'auth_provider', platform_required: true },
+    { label: 'Push Provider', value: 'push_provider', platform_required: true, screen: false },
+    { label: 'Ui Component', value: 'ui_component', platform_required: true, screen: false },
+    { label: 'Login', value: 'login', platform_required: true },
+    { label: 'Data Source Provider', value: 'data_source_provider', platform_required: false, screen: false },
+    { label: 'Broadcaster Selector', value: 'broadcaster_selector', platform_required: true },
+    { label: 'General', value: 'general', platform_required: true },
+    { label: 'Cell Style Family', value: 'cell_style_family', platform_required: true },
+    { label: 'Cell Builder', value: 'cell_builder', platform_required: true, screen: false },
+    { label: 'Video Advertisement', value: 'video_advertisement', platform_required: true, screen: false },
+    { label: 'Player Overlay', value: 'player_overlay', platform_required: false, screen: false }
+  ].freeze
 
   PLATFORMS = [
-    { label: "iOS", value: "ios" },
-    { label: "Android", value: "android" },
-    { label: "tvOS", value: "tvos" },
-    { label: "Android TV", value: "android_tv" },
-    { label: "Roku", value: "roku" },
-  ]
+    { label: 'iOS', value: 'ios' },
+    { label: 'Android', value: 'android' },
+    { label: 'tvOS', value: 'tvos' },
+    { label: 'Android TV', value: 'android_tv' },
+    { label: 'Roku', value: 'roku' }
+  ].freeze
 
-  INPUT_FIELD_TYPES = %i(text checkbox textarea dropdown tags colorpicker uploader)
+  INPUT_FIELD_TYPES = %i[text checkbox textarea dropdown tags colorpicker uploader].freeze
 
   TOOLTIP_TYPES = [
-    { type: :plain, value: "Plain text"},
-    { type: :url, value: "A link to an external resource"},
-    { type: :mixed, value: "Text with a link to an external resource"}
+    { type: :plain, value: 'Plain text' },
+    { type: :url, value: 'A link to an external resource' },
+    { type: :mixed, value: 'Text with a link to an external resource' }
   ].freeze
 
   module_function
 
   def create_file(manifest_hash)
-    File.open("plugin-manifest.json", "w") do |file|
+    File.open('plugin-manifest.json', 'w') do |file|
       file.write(JSON.pretty_generate(manifest_hash))
     end
   end
@@ -123,6 +124,6 @@ module ManifestHelpers
 
   def valid_account_ids?(plugin_version, new)
     return true unless new
-    plugin_version.manifest["whitelisted_account_ids"] && plugin_version.manifest["whitelisted_account_ids"].any?
+    plugin_version.manifest['whitelisted_account_ids'] && plugin_version.manifest['whitelisted_account_ids'].any?
   end
 end
